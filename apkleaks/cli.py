c8a5013d5869f9b4885b3896fb0a49c0e48d9192
wis4nggeni <becausewhynut1412@gmail.com>
2021-11-05 16:27:45 +0700

    Crime

--- apkleaks/cli.py
+++ apkleaks/cli.py
@@ -10,37 +10,38 @@ from apkleaks.apkleaks import APKLeaks
 from apkleaks.colors import color as col
 
 def header():
-	try:
-		VERSION = "v" + pkg_resources.require("apkleaks")[0].version
-	except Exception:
-		VERSION = open(os.path.join(str(Path(__file__).parent.parent), "VERSION"), "r").read().strip()
-	print(col.HEADER + "     _    ____  _  ___               _        \n    / \\  |  _ \\| |/ / |    ___  __ _| | _____ \n   / _ \\ | |_) | ' /| |   / _ \\/ _` | |/ / __|\n  / ___ \\|  __/| . \\| |__|  __/ (_| |   <\\__ \\\n /_/   \\_\\_|   |_|\\_\\_____\\___|\\__,_|_|\\_\\___/\n {}\n --\n Scanning APK file for URIs, endpoints & secrets\n (c) 2020-2021, dwisiswant0\n".format(VERSION) + col.ENDC, file=sys.stderr)
+    try:
+        VERSION = "v" + pkg_resources.require("apkleaks")[0].version
+    except Exception:
+        VERSION = open(os.path.join(str(Path(__file__).parent.parent), "VERSION"), "r").read().strip()
+    print(col.HEADER + "     _    ____  _  ___               _        \n    / \\  |  _ \\| |/ / |    ___  __ _| | _____ \n   / _ \\ | |_) | ' /| |   / _ \\/ _` | |/ / __|\n  / ___ \\|  __/| . \\| |__|  __/ (_| |   <\\__ \\\n /_/   \\_\\_|   |_|\\_\\_____\\___|\\__,_|_|\\_\\___/\n {}\n --\n Scanning APK file for URIs, endpoints & secrets\n (c) 2020-2021, dwisiswant0\n".format(VERSION) + col.ENDC, file=sys.stderr)
 
 def argument():
-	parser = argparse.ArgumentParser()
-	parser.add_argument("-f", "--file", help="APK file to scanning", type=str, required=True)
-	parser.add_argument("-o", "--output", help="Write to file results (random if not set)", type=str, required=False)
-	parser.add_argument("-p", "--pattern", help="Path to custom patterns JSON", type=str, required=False)
-	parser.add_argument("-a", "--args", help="Disassembler arguments (e.g. --threads-count 5 --deobf)", type=str, required=False)
-	parser.add_argument("--json", help="Save as JSON format", required=False, action="store_true")
-	arg = parser.parse_args()
-	return arg
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-f", "--file", help="APK file to scanning", type=str, required=True)
+    parser.add_argument("-o", "--output", help="Write to file results (random if not set)", type=str, required=False)
+    parser.add_argument("-p", "--pattern", help="Path to custom patterns JSON", type=str, required=False)
+    parser.add_argument("-a", "--args", help="Disassembler arguments (e.g. --threads-count 5 --deobf)", type=str, required=False)
+    parser.add_argument("--json", help="Save as JSON format", required=False, action="store_true")
+    arg = parser.parse_args()
+    return arg
 
 def main():
-	header()
-	args = argument()
-	a_file = open(args.file, "r")
-	for line in a_file:
-	    line = line.strip()
-	    print(args.file)
-	    parser = argparse.ArgumentParser()
-		arg = parser.parse_args()
+    header()
+    args = argument()
+    a_file = open(args.file, "r")
+    # noinspection PyInterpreter
+    for line in a_file:
+        line = line.strip()
+        print(args.file)
+        parser = argparse.ArgumentParser()
+        arg = parser.parse_args()
         arg.file = line+".apk"
         init = APKLeaks(arg)
-	    try:
-	        init.integrity()
-	        init.decompile()
-	        init.scanning()
-	    finally:
-	        init.cleanup()
-	a_file.close()
+        try:
+            init.integrity()
+            init.decompile()
+            init.scanning()
+        finally:
+            init.cleanup()
+    a_file.close()
